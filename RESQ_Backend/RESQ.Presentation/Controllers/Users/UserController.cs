26dd830b3dcc1d932de982316de038dad074ddcb
NTPAn <nguyenan19101112@gmail.com>
2026-01-19 16:20:48 +0700

    add base for user module

---
+++ RESQ_Backend/RESQ.Presentation/Controllers/Users/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESQ.Presentation.Controllers.Users
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
