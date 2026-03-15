4859e035f18b22661a916bba9bc43df04b190b5b
NTPAn <nguyenan19101112@gmail.com>
2026-03-15 17:47:04 +0700

    comment api that not use

--- RESQ_Backend/RESQ.Presentation/Controllers/Identity/RolesController.cs
+++ RESQ_Backend/RESQ.Presentation/Controllers/Identity/RolesController.cs
@@ -2,12 +2,8 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RESQ.Application.Common.Constants;
-using RESQ.Application.UseCases.Identity.Commands.CreateRole;
-using RESQ.Application.UseCases.Identity.Commands.DeleteRole;
 using RESQ.Application.UseCases.Identity.Commands.SetRolePermissions;
-using RESQ.Application.UseCases.Identity.Commands.UpdateRole;
 using RESQ.Application.UseCases.Identity.Queries.GetRolePermissions;
-using RESQ.Application.UseCases.Identity.Queries.GetRoles;
 
 namespace RESQ.Presentation.Controllers.Identity;
 
@@ -18,13 +14,13 @@ public class RolesController(IMediator mediator) : ControllerBase
 {
     private readonly IMediator _mediator = mediator;
 
-    /// <summary>Lấy danh sách tất cả role</summary>
-    [HttpGet]
-    public async Task<IActionResult> GetRoles()
-    {
-        var result = await _mediator.Send(new GetRolesQuery());
-        return Ok(result);
-    }
+    ///// <summary>Lấy danh sách tất cả role</summary>
+    //[HttpGet]
+    //public async Task<IActionResult> GetRoles()
+    //{
+    //    var result = await _mediator.Send(new GetRolesQuery());
+    //    return Ok(result);
+    //}
 
     /// <summary>Lấy danh sách permission của một role</summary>
     [HttpGet("{roleId:int}/permissions")]
@@ -34,31 +30,31 @@ public class RolesController(IMediator mediator) : ControllerBase
         return Ok(result);
     }
 
-    /// <summary>Tạo role mới</summary>
-    [HttpPost]
-    public async Task<IActionResult> CreateRole([FromBody] CreateRoleRequestDto dto)
-    {
-        var command = new CreateRoleCommand(dto.Name);
-        var result = await _mediator.Send(command);
-        return Ok(result);
-    }
+    ///// <summary>Tạo role mới</summary>
+    //[HttpPost]
+    //public async Task<IActionResult> CreateRole([FromBody] CreateRoleRequestDto dto)
+    //{
+    //    var command = new CreateRoleCommand(dto.Name);
+    //    var result = await _mediator.Send(command);
+    //    return Ok(result);
+    //}
 
-    /// <summary>Cập nhật tên role</summary>
-    [HttpPut("{roleId:int}")]
-    public async Task<IActionResult> UpdateRole(int roleId, [FromBody] UpdateRoleRequestDto dto)
-    {
-        var command = new UpdateRoleCommand(roleId, dto.Name);
-        var result = await _mediator.Send(command);
-        return Ok(result);
-    }
+    ///// <summary>Cập nhật tên role</summary>
+    //[HttpPut("{roleId:int}")]
+    //public async Task<IActionResult> UpdateRole(int roleId, [FromBody] UpdateRoleRequestDto dto)
+    //{
+    //    var command = new UpdateRoleCommand(roleId, dto.Name);
+    //    var result = await _mediator.Send(command);
+    //    return Ok(result);
+    //}
 
-    /// <summary>Xoá role</summary>
-    [HttpDelete("{roleId:int}")]
-    public async Task<IActionResult> DeleteRole(int roleId)
-    {
-        await _mediator.Send(new DeleteRoleCommand(roleId));
-        return NoContent();
-    }
+    ///// <summary>Xoá role</summary>
+    //[HttpDelete("{roleId:int}")]
+    //public async Task<IActionResult> DeleteRole(int roleId)
+    //{
+    //    await _mediator.Send(new DeleteRoleCommand(roleId));
+    //    return NoContent();
+    //}
 
     /// <summary>Gán/thay thế danh sách permission cho role</summary>
     [HttpPut("{roleId:int}/permissions")]
