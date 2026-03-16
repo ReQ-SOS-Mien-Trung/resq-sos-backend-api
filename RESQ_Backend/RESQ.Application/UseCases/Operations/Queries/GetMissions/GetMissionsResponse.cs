fb1d1381786e938119e12d8930b3641c2218a88a
NTPAn <nguyenan19101112@gmail.com>
2026-03-16 23:26:30 +0700

    add team location in get mission api

--- RESQ_Backend/RESQ.Application/UseCases/Operations/Queries/GetMissions/GetMissionsResponse.cs
+++ RESQ_Backend/RESQ.Application/UseCases/Operations/Queries/GetMissions/GetMissionsResponse.cs
@@ -148,9 +148,14 @@ public class AssignedTeamDto
     public int RescueTeamId { get; set; }
     public string? TeamName { get; set; }
     public string? TeamCode { get; set; }
+    public string? AssemblyPointName { get; set; }
     public string? TeamType { get; set; }
     public string? Status { get; set; }
     public string? Note { get; set; }
+    public double? Latitude { get; set; }
+    public double? Longitude { get; set; }
+    public DateTime? LocationUpdatedAt { get; set; }
+    public string? LocationSource { get; set; }
     public DateTime? AssignedAt { get; set; }
     public DateTime? UnassignedAt { get; set; }
 }
