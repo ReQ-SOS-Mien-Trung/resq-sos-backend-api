e8556b097ae4025ac336161295b5e5690303b002
NTPAn <nguyenan19101112@gmail.com>
2026-04-01 17:31:39 +0700

    add reportBy full infor in incident response

---
+++ RESQ_Backend/RESQ.Application/Common/Models/ReportedByDto.cs
@@ -0,0 +1,11 @@
+namespace RESQ.Application.Common.Models;
+
+public class ReportedByDto
+{
+    public Guid Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public string? AvatarUrl { get; set; }
+}
--- RESQ_Backend/RESQ.Application/UseCases/Operations/Queries/GetTeamIncidents/GetTeamIncidentsQueryHandler.cs
+++ RESQ_Backend/RESQ.Application/UseCases/Operations/Queries/GetTeamIncidents/GetTeamIncidentsQueryHandler.cs
@@ -1,29 +1,62 @@
 using MediatR;
+using RESQ.Application.Common.Models;
+using RESQ.Application.Repositories.Identity;
 using RESQ.Application.Repositories.Operations;
 
 namespace RESQ.Application.UseCases.Operations.Queries.GetTeamIncidents;
 
 public class GetTeamIncidentsQueryHandler(
-    ITeamIncidentRepository teamIncidentRepository
+    ITeamIncidentRepository teamIncidentRepository,
+    IUserRepository userRepository
 ) : IRequestHandler<GetTeamIncidentsQuery, GetTeamIncidentsResponse>
 {
     public async Task<GetTeamIncidentsResponse> Handle(GetTeamIncidentsQuery request, CancellationToken cancellationToken)
     {
         var incidents = await teamIncidentRepository.GetByMissionIdAsync(request.MissionId, cancellationToken);
+        var incidentList = incidents.ToList();
+
+        var reporterIds = incidentList
+            .Where(i => i.ReportedBy.HasValue)
+            .Select(i => i.ReportedBy!.Value)
+            .Distinct()
+            .ToList();
+
+        var userTasks = reporterIds.Select(id => userRepository.GetByIdAsync(id, cancellationToken));
+        var users = await Task.WhenAll(userTasks);
+        var userLookup = users
+            .Where(u => u is not null)
+            .ToDictionary(u => u!.Id, u => u!);
 
         return new GetTeamIncidentsResponse
         {
             MissionId = request.MissionId,
-            Incidents = incidents.Select(i => new TeamIncidentDto
+            Incidents = incidentList.Select(i =>
             {
-                IncidentId = i.Id,
-                MissionTeamId = i.MissionTeamId,
-                Latitude = i.Latitude,
-                Longitude = i.Longitude,
-                Description = i.Description,
-                Status = i.Status.ToString(),
-                ReportedBy = i.ReportedBy,
-                ReportedAt = i.ReportedAt
+                ReportedByDto? reportedBy = null;
+                if (i.ReportedBy.HasValue && userLookup.TryGetValue(i.ReportedBy.Value, out var user))
+                {
+                    reportedBy = new ReportedByDto
+                    {
+                        Id        = user.Id,
+                        FirstName = user.FirstName,
+                        LastName  = user.LastName,
+                        Phone     = user.Phone,
+                        Email     = user.Email,
+                        AvatarUrl = user.AvatarUrl
+                    };
+                }
+
+                return new TeamIncidentDto
+                {
+                    IncidentId    = i.Id,
+                    MissionTeamId = i.MissionTeamId,
+                    Latitude      = i.Latitude,
+                    Longitude     = i.Longitude,
+                    Description   = i.Description,
+                    Status        = i.Status.ToString(),
+                    ReportedBy    = reportedBy,
+                    ReportedAt    = i.ReportedAt
+                };
             }).ToList()
         };
     }
