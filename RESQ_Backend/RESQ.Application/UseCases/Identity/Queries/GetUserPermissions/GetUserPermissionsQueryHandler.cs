0ee87b60b56f36ff183b54b813818251f6b5b7bb
NTPAn <nguyenan19101112@gmail.com>
2026-04-19 22:20:58 +0700

    feat: Add role permissions to user permissions query handler and response

--- RESQ_Backend/RESQ.Application/UseCases/Identity/Queries/GetUserPermissions/GetUserPermissionsQueryHandler.cs
+++ RESQ_Backend/RESQ.Application/UseCases/Identity/Queries/GetUserPermissions/GetUserPermissionsQueryHandler.cs
@@ -6,11 +6,13 @@ namespace RESQ.Application.UseCases.Identity.Queries.GetUserPermissions;
 
 public class GetUserPermissionsQueryHandler(
     IUserRepository userRepository,
-    IPermissionRepository permissionRepository
+    IPermissionRepository permissionRepository,
+    IRoleRepository roleRepository
 ) : IRequestHandler<GetUserPermissionsQuery, GetUserPermissionsResponse>
 {
     private readonly IUserRepository _userRepository = userRepository;
     private readonly IPermissionRepository _permissionRepository = permissionRepository;
+    private readonly IRoleRepository _roleRepository = roleRepository;
 
     public async Task<GetUserPermissionsResponse> Handle(GetUserPermissionsQuery request, CancellationToken cancellationToken)
     {
@@ -18,11 +20,22 @@ public class GetUserPermissionsQueryHandler(
             ?? throw new NotFoundException($"Không tìm thấy user với ID {request.UserId}");
 
         var permissions = await _permissionRepository.GetUserPermissionsAsync(user.Id, cancellationToken);
+        var rolePermissions = user.RoleId.HasValue
+            ? await _roleRepository.GetPermissionsAsync(user.RoleId.Value, cancellationToken)
+            : [];
 
         return new GetUserPermissionsResponse
         {
             UserId = user.Id,
+            RoleId = user.RoleId,
             Permissions = permissions.Select(p => new UserPermissionDto
+            {
+                Id = p.Id,
+                Code = p.Code,
+                Name = p.Name,
+                Description = p.Description
+            }).ToList(),
+            RolePermissions = rolePermissions.Select(p => new UserPermissionDto
             {
                 Id = p.Id,
                 Code = p.Code,
