002be407ed84f0f39386b9de62e6d2f5aa92ffa7
BeoChu <chouladila@gmail.com>
2026-04-26 23:01:18 +0700

    chore: migration

---
+++ RESQ_Backend/RESQ.Infrastructure/Migrations/20260426160023_AddNewSeeds1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RESQ.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewSeeds1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
