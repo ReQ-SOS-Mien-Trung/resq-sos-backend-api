0ba9a4a19e23095371a623a92305369cb4bd831a
BeoChu <chouladila@gmail.com>
2026-05-07 20:33:07 +0700

    chore: migration

--- RESQ_Backend/RESQ.Infrastructure/Migrations/20260507133036_AddNewSeeds.Designer.cs
+++ RESQ_Backend/RESQ.Infrastructure/Migrations/20260507133036_AddNewSeeds.Designer.cs
@@ -13,7 +13,7 @@ using RESQ.Infrastructure.Persistence.Context;
 namespace RESQ.Infrastructure.Migrations
 {
     [DbContext(typeof(ResQDbContext))]
-    [Migration("20260507113220_AddNewSeeds")]
+    [Migration("20260507133036_AddNewSeeds")]
     partial class AddNewSeeds
     {
         /// <inheritdoc />
